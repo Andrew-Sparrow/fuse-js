28ebf4a190e983acaf66a50be6c122321e88891e
Andrew <8911102@gmail.com>
2021-03-30 12:13:14 +0500

    problem with img copying to dist was resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import chevronPath from "./img/chevron.svg";
+import img1 from "./img/img-1.jpg";
+import img2 from "./img/img-2.jpg";
+import img3 from "./img/img-3.jpg";
 import "./sass/style.scss";
 
 console.log("success!");
-console.log(chevronPath);
