b01e144d296749195d1362f546215c1a492a4472
Andrew <8911102@gmail.com>
2021-04-04 22:51:05 +0500

    id was added to card link

--- src/js/utils/consts.js
+++ src/js/utils/consts.js
@@ -1,14 +1,5 @@
 export const CARDS_COUNT_PER_STEP = 6;
 
-export const BASE = 10; // the base in mathematical numeral systems
-
-export const UserActionForModel = {
-  ADD_ITEM: `ADD_ITEM`,
-  DELETE_ITEM: `DELETE_ITEM`,
-  UPDATE_ITEM: `UPDATE_ITEM`,
-  UPDATE_COMMENTS: `UPDATE_COMMENTS`
-};
-
 export const UpdateTypeForRerender = {
   MAJOR: `MAJOR`,
   INIT: `INIT`,
--- src/js/view/cards/card-view.js
+++ src/js/view/cards/card-view.js
@@ -3,6 +3,7 @@ import {numberWithCommas} from "../../utils/utils";
 
 const createFilmCardTemplate = (card) => {
   const {
+    id,
     title,
     address,
     type,
@@ -10,7 +11,7 @@ const createFilmCardTemplate = (card) => {
   } = card;
 
   return `<li class="card">
-            <a class="card__link" href="#">
+            <a class="card__link" href="/details/${id}">
               <img class="card__house-picture" src="images/img-1.jpg" width="200" height="150" alt="picture of house" aria-label="picture of house">
               <div class="card__description card__description_${type.toLowerCase()}">
                 <h3 class="card__title">${title}</h3>
