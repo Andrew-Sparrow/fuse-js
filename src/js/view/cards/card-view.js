ce08f596173c9bc892d519f53eeb9ba65e1e48b4
Andrew <8911102@gmail.com>
2021-04-04 22:28:11 +0500

    label was added

--- src/js/view/cards/card-view.js
+++ src/js/view/cards/card-view.js
@@ -8,16 +8,16 @@ const createFilmCardTemplate = (card) => {
     price
   } = card;
 
-  return `<li class="card card__${type.toLowerCase()}">
+  return `<li class="card">
             <a class="card__link" href="#">
               <img class="card__house-picture" src="images/img-1.jpg" width="200" height="150" alt="picture of house" aria-label="picture of house">
-              <div class="card__description card__description_independent">
+              <div class="card__description card__description_${type.toLowerCase()}">
                 <h3 class="card__title">${title}</h3>
                 <p class="card__address">
                   ${address}
                 </p>
                 <p class="card__properties">
-                  New Properties for Sale from <span class="card__properties_price">${price}</span>
+                  New Properties for Sale from <span class="card__properties_sign">£</span><span class="card__properties_price">${price}</span>
                 </p>
                 <p class="card__shared">Shared Ownership Available</p>
               </div>
